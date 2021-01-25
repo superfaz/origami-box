3f3bf565810dd8eaf60fa2c7ac843c7f8a41e788
superfaz <16510828+superfaz@users.noreply.github.com>
2021-01-25 21:59:22 +0100

    Fix the known languages

--- src/i18n.js
+++ src/i18n.js
@@ -17,6 +17,7 @@ i18n
   .init({
     fallbackLng: 'en',
     debug: true,
+    supportedLngs: ['en', 'fr'],
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
