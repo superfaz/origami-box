ce51c7f772e17eef0a05005a3ba996eef8d857d5
superfaz <16510828+superfaz@users.noreply.github.com>
2021-02-08 22:08:22 +0100

    Code review

--- src/store.js
+++ src/store.js
@@ -1,36 +1,7 @@
 import { combineReducers, createStore } from 'redux';
 import masuReducer from './Masu/reducer';
 
-const initialState = {
-};
-
-/* export function changeTheme(theme) {
-    if (typeof(theme) == 'object') {
-        theme = theme.target.checked ? 'light' : 'dark';
-    }
-
-    return ({ type: 'CHANGE_THEME', payload: theme });
-}
-
-export function getTheme(store) {
-    return store.userPreferences.theme;
-}
- */
-
-function userPreferences(state = initialState, action) {
-    switch (action.type) {
-/*         case 'CHANGE_THEME':
-            return {
-                ...state,
-                theme: action.payload
-            };
- */        default:
-            return state;
-    }
-}
-
 const reducers = combineReducers({
-    userPreferences,
     masu: masuReducer,
 });
 
