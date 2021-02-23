0c0832309ac4d360e962d532eaff663c15b18301
superfaz <16510828+superfaz@users.noreply.github.com>
2021-02-23 21:43:39 +0100

    Print - Add page size debug

--- src/Masu/MasuTemplateFront.js
+++ src/Masu/MasuTemplateFront.js
@@ -53,6 +53,17 @@ export default function MasuTemplateFront({ lid = false, print = false }) {
         </g>
       }
 
+      {process.env.REACT_APP_SVG_DEBUG &&
+        <g transform={`translate(${-pageWidth / 2} ${-pageLength / 2})`}>
+          <line x1={0} y1={5} x2={pageWidth} y2={5} style={{ stroke: 'gray', strokeWidth: 0.2 }} />
+          <line x1={5} y1={0} x2={5} y2={pageLength} style={{ stroke: 'gray', strokeWidth: 0.2 }} />
+          <circle cx={0} cy={5} r={1} style={{ fill: 'red' }} />
+          <circle cx={210} cy={5} r={1} style={{ fill: 'red' }} />
+          <circle cx={5} cy={0} r={1} style={{ fill: 'red' }} />
+          <circle cx={5} cy={297} r={1} style={{ fill: 'red' }} />
+        </g>
+      }
+
       <g transform="rotate(45)">
         <SvgCut m={m} styles={styles} />
       </g>
