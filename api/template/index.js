8f9400a83b3ad47be5460248ee3f96cfc8a8fc70
superfaz <16510828+superfaz@users.noreply.github.com>
2021-03-05 12:00:56 +0100

    API - retrieve all templates

--- api/template/index.js
+++ api/template/index.js
@@ -1,14 +1,44 @@
+const axios = require('axios').default;
 const { MongoClient } = require("mongodb");
+
+function assert(value, message) {
+  if (value === undefined || value === null) {
+    throw new Error(message);
+  }
+
+  return value;
+}
 
 module.exports = async function (context, req) {
   context.log('/api/template call');
 
-  const uri = process.env.MONGO_CONNECTION_STRING;
-  if (uri === undefined || uri === null) {
-    throw new Error('MONGO_CONNECTION_STRING not defined');
+  const mongoUri = assert(process.env.MONGO_CONNECTION_STRING, 'MONGO_CONNECTION_STRING not defined');
+  const facebookAppId = assert(process.env.FACEBOOK_APP_ID, 'FACEBOOK_APP_ID not defined');
+  const facebookAppSecret = assert(process.env.FACEBOOK_APP_SECRET, 'FACEBOOK_APP_SECRET not defined');
+  const accessToken = assert(req.headers.accesstoken, 'Missing accesstoken in the request headers');
+  const userId = assert(req.headers.userid, 'Missing userid in the request headers');
+
+  const response = await axios.get('https://graph.facebook.com/debug_token', {
+    params: {
+      input_token: accessToken,
+      access_token: `${facebookAppId}|${facebookAppSecret}`,
+    }
+  });
+
+  // Confirm that the response is valid
+  if (response.status !== 200
+    && response.data.data.app_id !== facebookAppId) {
+    throw new Error('Invalid response');
   }
 
-  const client = new MongoClient(uri, {
+  // Confirm that the user is valid
+  if (response.data.data.is_valid !== true
+    && response.data.data.app_id !== facebookAppId
+    && response.data.data.user_id !== userId) {
+    throw new Error('Invalid user');
+  }
+
+  const client = new MongoClient(mongoUri, {
     useUnifiedTopology: true
   });
 
@@ -18,7 +48,7 @@ module.exports = async function (context, req) {
     const database = client.db('application');
     const templates = database.collection('templates');
 
-    const query = { type: 'masu' };
+    const query = { userId: userId };
     const results = templates.find(query);
 
     context.res = {
