7e0d9134810c0f5c36f8a7f9355f0a4382125dd1
superfaz <16510828+superfaz@users.noreply.github.com>
2021-03-06 08:48:14 +0100

    API - Improve error / log management

--- api/template/index.js
+++ api/template/index.js
@@ -1,9 +1,23 @@
 const axios = require('axios').default;
 const { MongoClient } = require("mongodb");
 
-function assert(value, message) {
+class SystemError extends Error {
+}
+
+class UserError extends Error {
+}
+
+function assertSystem(value, message) {
   if (value === undefined || value === null) {
-    throw new Error(message);
+    throw new SystemError(message);
+  }
+
+  return value;
+}
+
+function assertUser(value, message) {
+  if (value === undefined || value === null) {
+    throw new UserError(message);
   }
 
   return value;
@@ -11,13 +25,13 @@ function assert(value, message) {
 
 module.exports = async function (context, req) {
   try {
-    context.log('/api/template call');
+    context.log.info('/api/template call');
 
-    const mongoUri = assert(process.env.MONGO_CONNECTION_STRING, 'MONGO_CONNECTION_STRING not defined');
-    const facebookAppId = assert(process.env.FACEBOOK_APP_ID, 'FACEBOOK_APP_ID not defined');
-    const facebookAppSecret = assert(process.env.FACEBOOK_APP_SECRET, 'FACEBOOK_APP_SECRET not defined');
-    const accessToken = assert(req.headers.accesstoken, 'Missing accesstoken in the request headers');
-    const userId = assert(req.headers.userid, 'Missing userid in the request headers');
+    const mongoUri = assertSystem(process.env.MONGO_CONNECTION_STRING, "MONGO_CONNECTION_STRING not defined");
+    const facebookAppId = assertSystem(process.env.FACEBOOK_APP_ID, "FACEBOOK_APP_ID not defined");
+    const facebookAppSecret = assertSystem(process.env.FACEBOOK_APP_SECRET, "FACEBOOK_APP_SECRET not defined");
+    const accessToken = assertUser(req.headers.accesstoken, "Missing 'accesstoken' in the request headers");
+    const userId = assertUser(req.headers.userid, "Missing 'userid' in the request headers");
 
     const response = await axios.get('https://graph.facebook.com/debug_token', {
       params: {
@@ -29,14 +43,14 @@ module.exports = async function (context, req) {
     // Confirm that the response is valid
     if (response.status !== 200
       && response.data.data.app_id !== facebookAppId) {
-      throw new Error('Invalid response');
+      throw new SystemError("Can't connect to facebook APIs");
     }
 
     // Confirm that the user is valid
     if (response.data.data.is_valid !== true
       && response.data.data.app_id !== facebookAppId
       && response.data.data.user_id !== userId) {
-      throw new Error('Invalid user');
+      throw new UserError("Invalid user");
     }
 
     const client = new MongoClient(mongoUri, {
@@ -60,13 +74,31 @@ module.exports = async function (context, req) {
     finally {
       await client.close();
     }
-
   }
   catch (error) {
-    context.log('error', error);
-    context.res = {
-      status: 400,
-      body: { error: error.message }
+    if (error instanceof UserError) {
+      context.log.warn('User request invalid', error);
+      context.res = {
+        status: 400,
+        body: { error: error.message }
+      }
     }
+    else if (error instanceof SystemError) {
+      context.log.error('Configuration issue', error);
+      context.res = {
+        status: 500,
+        body: { error: 'Misconfiguration of the API' }
+      }
+    }
+    else {
+      context.log.error('Unplanned error', error);
+      context.res = {
+        status: 500,
+        body: { error: 'System error in the API' }
+      }
+    }
+  }
+  finally {
+    context.done();
   }
 }
