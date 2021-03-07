a9c4ab94ab8b1990f27e03b30ffdcaa1715bd628
superfaz <16510828+superfaz@users.noreply.github.com>
2021-03-07 18:18:19 +0100

    Simplify process page

--- src/Process.js
+++ src/Process.js
@@ -1,19 +1,10 @@
-import React from 'react';
+import JsonDisplay from './Generic/JsonDisplay';
 
 export default function Process() {
-  const keys = Object.keys(process.env);
-  const values = keys
-    .map((key, index) => [
-      { type: 'dt', key: 't' + index, value: key },
-      { type: 'dd', key: 'd' + index, value: process.env[key] }
-    ]).flat();
   return (
     <div className="container">
       <h1>Defined process variables</h1>
-      <dl>
-        {values.map(item =>
-          React.createElement(item.type, { key: item.key }, item.value))}
-      </dl>
+      <JsonDisplay json={process.env} title="process.env" />
     </div>
   );
 }
