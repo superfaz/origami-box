04505c349e331215b611ee6bd303129bc438ffbd
superfaz <16510828+superfaz@users.noreply.github.com>
2021-03-09 09:53:18 +0100

    Code review

--- src/objectMap.js
+++ src/objectMap.js
@@ -1,6 +1,6 @@
 export default function objectMap(obj, action = () => { }) {
   if (obj === undefined || obj === null) {
-    return;
+    return null;
   }
 
   return Object.keys(obj).map((key, index) => { return action(obj[key], key, index, obj) });
