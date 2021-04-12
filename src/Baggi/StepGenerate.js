ad94073bb43f2a436cee1a4a50c78552c4cfbdf3
superfaz <16510828+superfaz@users.noreply.github.com>
2021-04-12 14:12:59 +0200

    Baggi - Add verso to print preview

--- src/Baggi/StepGenerate.js
+++ src/Baggi/StepGenerate.js
@@ -1,12 +1,16 @@
 import BaseStepGenerate from "../BaseTemplate/BaseStepGenerate";
 import BaggiTemplateRecto from "./BaggiTemplateRecto";
+import BaggiTemplateVerso from "./BaggiTemplateVerso";
 
 export default function StepGenerate() {
   return (
     <BaseStepGenerate>
       <div className="row">
         <div className="col-12 col-lg-6 mb-3">
-          <BaggiTemplateRecto print="true" />
+          <BaggiTemplateRecto print={true} />
+        </div>
+        <div className="col-12 col-lg-6 mb-3">
+          <BaggiTemplateVerso print={true} />
         </div>
       </div>
     </BaseStepGenerate>
