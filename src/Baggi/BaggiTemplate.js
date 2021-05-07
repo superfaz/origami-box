cd30230ad2998ca6e9e1411fde794f1f7c1d4e62
superfaz <16510828+superfaz@users.noreply.github.com>
2021-05-07 22:44:11 +0200

    BaggiTemplate - manage background color for side representation

--- src/Baggi/BaggiTemplate.js
+++ src/Baggi/BaggiTemplate.js
@@ -56,6 +56,22 @@ export default function BaggiTemplate({
       <SvgClipPaths ids={ids} faces={faces} side="verso" />
 
       <defs>
+        <rect
+          id="rectoColor"
+          x={-d.width / 2}
+          y={-d.height / 2}
+          width={d.width}
+          height={4.0 * d.w}
+          fill={blockData.rectoColor}
+        />
+        <rect
+          id="versoColor"
+          x={-d.width / 2}
+          y={-d.height / 2}
+          width={d.width}
+          height={4.0 * d.w}
+          fill={blockData.versoColor}
+        />
         {Boolean(blockData.rectoImage) && (
           <image
             id="rectoImage"
@@ -98,100 +114,86 @@ export default function BaggiTemplate({
       ))}
 
       <defs>
+        <polygon
+          id="side-0"
+          points={[
+            [-d.w_2, 0],
+            [0, -d.w_2],
+            [0, d.w_2],
+          ].join(" ")}
+        />
+        <polygon
+          id="side-1"
+          points={[
+            [0, d.w_2],
+            [-d.w, d.w_2],
+            [-d.w, -d.w_2],
+          ].join(" ")}
+        />
+        <polygon
+          id="side-2"
+          points={[
+            [-d.w, -d.w_2],
+            [0, -d.w_2],
+            [-d.w_2, 0],
+          ].join(" ")}
+        />
         <clipPath id={ids.unique("left-0")}>
-          <polygon
-            points={[
-              [-d.l_2 - d.w_2, 0],
-              [-d.l_2, -d.w_2],
-              [-d.l_2, d.w_2],
-            ].join(" ")}
-          />
+          <use href="#side-0" transform={`translate(${-d.l_2} 0)`} />
         </clipPath>
         <clipPath id={ids.unique("left-1")}>
-          <polygon
-            points={[
-              [-d.l_2, d.w_2],
-              [-d.l_2 - d.w, d.w_2],
-              [-d.l_2 - d.w, -d.w_2],
-            ].join(" ")}
-          />
+          <use href="#side-1" transform={`translate(${-d.l_2} 0)`} />
         </clipPath>
         <clipPath id={ids.unique("left-2")}>
-          <polygon
-            points={[
-              [-d.l_2 - d.w, -d.w_2],
-              [-d.l_2, -d.w_2],
-              [-d.l_2 - d.w_2, 0],
-            ].join(" ")}
-          />
+          <use href="#side-2" transform={`translate(${-d.l_2} 0)`} />
         </clipPath>
         <clipPath id={ids.unique("right-0")}>
-          <polygon
-            points={[
-              [d.l_2 + d.w_2, 0],
-              [d.l_2, -d.w_2],
-              [d.l_2, d.w_2],
-            ].join(" ")}
-          />
+          <use href="#side-0" transform={`translate(${d.l_2} 0) scale(-1 1)`} />
         </clipPath>
         <clipPath id={ids.unique("right-1")}>
-          <polygon
-            points={[
-              [d.l_2, d.w_2],
-              [d.l_2 + d.w, d.w_2],
-              [d.l_2 + d.w, -d.w_2],
-            ].join(" ")}
-          />
+          <use href="#side-1" transform={`translate(${d.l_2} 0) scale(-1 1)`} />
         </clipPath>
         <clipPath id={ids.unique("right-2")}>
-          <polygon
-            points={[
-              [d.l_2 + d.w, -d.w_2],
-              [d.l_2, -d.w_2],
-              [d.l_2 + d.w_2, 0],
-            ].join(" ")}
-          />
+          <use href="#side-2" transform={`translate(${d.l_2} 0) scale(-1 1)`} />
         </clipPath>
       </defs>
 
-      {Boolean(blockData.rectoImage) && (
-        <>
-          <g clipPath={"url(#" + ids.unique("left-0") + ")"}>
-            <use href="#rectoImage" />
-          </g>
-          <g clipPath={"url(#" + ids.unique("right-0") + ")"}>
-            <use href="#rectoImage" />
-          </g>
-          <g clipPath={"url(#" + ids.unique("left-2") + ")"}>
-            <use
-              href="#rectoImage"
-              transform={`rotate(-90 ${-d.l_2} ${-d.w_2})`}
-            />
-          </g>
-          <g clipPath={"url(#" + ids.unique("right-2") + ")"}>
-            <use
-              href="#rectoImage"
-              transform={`rotate(90 ${d.l_2} ${-d.w_2})`}
-            />
-          </g>
-        </>
-      )}
-      {Boolean(blockData.versoImage) && (
-        <>
-          <g clipPath={"url(#" + ids.unique("left-1") + ")"}>
-            <use
-              href="#versoImage"
-              transform={`rotate(90 ${-d.l_2} ${d.w_2})`}
-            />
-          </g>
-          <g clipPath={"url(#" + ids.unique("right-1") + ")"}>
-            <use
-              href="#versoImage"
-              transform={`rotate(-90 ${d.l_2} ${d.w_2})`}
-            />
-          </g>
-        </>
-      )}
+      <g clipPath={"url(#" + ids.unique("left-0") + ")"}>
+        <use href="#rectoColor" />
+        {blockData.rectoImage && <use href="#rectoImage" />}
+      </g>
+      <g clipPath={"url(#" + ids.unique("right-0") + ")"}>
+        <use href="#rectoColor" />
+        {blockData.rectoImage && <use href="#rectoImage" />}
+      </g>
+      <g clipPath={"url(#" + ids.unique("left-2") + ")"}>
+        <use href="#rectoColor" />
+        {blockData.rectoImage && (
+          <use
+            href="#rectoImage"
+            transform={`rotate(-90 ${-d.l_2} ${-d.w_2})`}
+          />
+        )}
+      </g>
+
+      <g clipPath={"url(#" + ids.unique("right-2") + ")"}>
+        <use href="#rectoColor" />
+        {blockData.rectoImage && (
+          <use href="#rectoImage" transform={`rotate(90 ${d.l_2} ${-d.w_2})`} />
+        )}
+      </g>
+      <g clipPath={"url(#" + ids.unique("left-1") + ")"}>
+        <use href="#versoColor" />
+        {blockData.rectoImage && (
+          <use href="#versoImage" transform={`rotate(90 ${-d.l_2} ${d.w_2})`} />
+        )}
+      </g>
+      <g clipPath={"url(#" + ids.unique("right-1") + ")"}>
+        <use href="#versoColor" />
+        {blockData.rectoImage && (
+          <use href="#versoImage" transform={`rotate(-90 ${d.l_2} ${d.w_2})`} />
+        )}
+      </g>
 
       <SvgFacesContent
         ids={ids}
