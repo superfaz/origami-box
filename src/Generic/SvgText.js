ae51bfd1881ecf1d467b5c6a80f4e54d2a56350c
superfaz <16510828+superfaz@users.noreply.github.com>
2021-05-13 23:15:39 +0200

    Simplify text box calculation

--- src/Generic/SvgText.js
+++ src/Generic/SvgText.js
@@ -1,32 +1,43 @@
-import { useEffect, useRef, useState } from "react";
 import env from "../env";
+
+const canvas = document.createElement("canvas");
+const context = canvas.getContext("2d");
+const pixelsPerMillimeters = 3.779527559055118110236220472;
 
 export function configurePositioning(face, text) {
   const margins = {
-    hori: parseFloat(text.marginHorizontal),
-    vert: parseFloat(text.marginVertical),
+    hori: parseFloat(text.marginHorizontal) || 0,
+    vert: parseFloat(text.marginVertical) || 0,
   };
-  if (isNaN(margins.hori)) {
-    margins.hori = 0;
-  }
-  if (isNaN(margins.vert)) {
-    margins.vert = 0;
-  }
 
   let style = {};
   let configuration = { ...face };
+
+  context.font = `${text.size}mm ${text.family}`;
+  const measure = context.measureText(text.content);
+  configuration.width =
+    Math.max(
+      ...text.content.split("\n").map((line) => context.measureText(line).width)
+    ) / pixelsPerMillimeters;
+  configuration.height =
+    (text.content.split("\n").length *
+      (measure.fontBoundingBoxAscent + measure.fontBoundingBoxDescent)) /
+    pixelsPerMillimeters;
 
   switch (text.horizontal) {
     case "left":
       style.textAnchor = "start";
       configuration.x -= configuration.hori - margins.hori;
+      configuration.dx = configuration.x + configuration.width / 2;
       break;
     case "center":
       style.textAnchor = "middle";
+      configuration.dx = configuration.x;
       break;
     case "right":
       style.textAnchor = "end";
       configuration.x += configuration.hori - margins.hori;
+      configuration.dx = configuration.x - configuration.width / 2;
       break;
     default:
       throw new Error(
@@ -38,13 +49,16 @@ export function configurePositioning(face, text) {
     case "top":
       style.dominantBaseline = "text-before-edge";
       configuration.y -= configuration.vert - margins.vert;
+      configuration.dy = configuration.y + configuration.height / 2;
       break;
     case "middle":
       style.dominantBaseline = "central";
+      configuration.dy = configuration.y;
       break;
     case "bottom":
       style.dominantBaseline = "text-after-edge";
       configuration.y += configuration.vert - margins.vert;
+      configuration.dy = configuration.y - configuration.height / 2;
       break;
     default:
       throw new Error(`The vertical value '${text.vertical}' is not managed`);
@@ -54,9 +68,6 @@ export function configurePositioning(face, text) {
 }
 
 export default function SvgText({ face, text }) {
-  const textRef = useRef(null);
-  const [box, setBox] = useState(null);
-
   let { configuration, style } = configurePositioning(face, text);
   style.fontSize = text.size;
   style.fill = text.color;
@@ -64,14 +75,6 @@ export default function SvgText({ face, text }) {
   if (text.family !== "") {
     style.fontFamily = text.family;
   }
-
-  // Used for debugging only - see REACT_APP_SVG_DEBUG
-  useEffect(() => setBox(textRef.current?.getBBox()), [
-    textRef,
-    text,
-    text.content,
-    text.family,
-  ]);
 
   // Multiline management - required as SVG doesn't support it and only align the baseline.
   const lines = text.content.split("\n");
@@ -92,18 +95,18 @@ export default function SvgText({ face, text }) {
 
   return (
     <g>
-      {box && env.debug.svg && (
+      {env.debug.svg && (
         <rect
-          x={box.x}
-          y={box.y}
-          width={box.width}
-          height={box.height}
+          x={configuration.dx - configuration.width / 2}
+          y={configuration.dy - configuration.height / 2}
+          width={configuration.width}
+          height={configuration.height}
           style={{ strokeWidth: 0.2 }}
           stroke="black"
           fill="yellow"
         />
       )}
-      <text ref={textRef} style={style} x={configuration.x} y={configuration.y}>
+      <text style={style} x={configuration.x} y={configuration.y}>
         {lines.map((line, index) => {
           return (
             <tspan
