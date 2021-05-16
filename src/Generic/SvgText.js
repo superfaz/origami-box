85756a5ca622a087854072195c4a5ed7a698fa6c
superfaz <16510828+superfaz@users.noreply.github.com>
2021-05-16 08:32:52 +0200

    Correct text positioning with rotation

--- src/Generic/SvgText.js
+++ src/Generic/SvgText.js
@@ -4,11 +4,34 @@ const canvas = document.createElement("canvas");
 const context = canvas.getContext("2d");
 const pixelsPerMillimeters = 3.779527559055118110236220472;
 
+/**
+ * @typedef {Object} Positioning
+ * @property {number} x - The x coordinate to position the text
+ * @property {number} y - The y coordinate to position the text
+ * @property {number} cx - The x coordinate representing the center of the text area;
+ *                         To be used to apply the rotation
+ * @property {number} cy - The y coordinate representing the center of the text area;
+ *                         To be used to apply the rotation
+ * @property {number} width - The width of the text area
+ * @property {number} height - The height of the text area
+ */
+
+/**
+ * Computes different values for the positioning and styling of a text.
+ * @param {Object} face The detail about the face on which the text is displayed
+ * @param {Object} text The detail about the text to display
+ * @returns {Object}
+ * @returns {Positioning} configuration Positioning information for this text
+ * @returns {Style} style Style to be applied to this text to ensure proper positioning
+ */
 export function configurePositioning(face, text) {
   const margins = {
     hori: parseFloat(text.marginHorizontal) || 0,
     vert: parseFloat(text.marginVertical) || 0,
   };
+  const rotation = parseFloat(text.rotation) || 0;
+  const abscos = Math.abs(Math.cos(rotation * (Math.PI / 180.0)));
+  const abssin = Math.abs(Math.sin(rotation * (Math.PI / 180.0)));
 
   let style = {};
   let configuration = { ...face };
@@ -24,11 +47,20 @@ export function configurePositioning(face, text) {
       (measure.fontBoundingBoxAscent + measure.fontBoundingBoxDescent)) /
     pixelsPerMillimeters;
 
+  var rotatedWidth =
+    configuration.width * abscos + configuration.height * abssin;
+  var rotatedHeight =
+    configuration.width * abssin + configuration.height * abscos;
+  var diffWidth = rotatedWidth - configuration.width;
+  var diffHeight = rotatedHeight - configuration.height;
+
   switch (text.horizontal) {
     case "left":
       style.textAnchor = "start";
       configuration.x -= configuration.hori - margins.hori;
       configuration.cx = configuration.x + configuration.width / 2;
+      configuration.x += diffWidth / 2;
+      configuration.cx += diffWidth / 2;
       break;
     case "center":
       style.textAnchor = "middle";
@@ -38,6 +70,8 @@ export function configurePositioning(face, text) {
       style.textAnchor = "end";
       configuration.x += configuration.hori - margins.hori;
       configuration.cx = configuration.x - configuration.width / 2;
+      configuration.x -= diffWidth / 2;
+      configuration.cx -= diffWidth / 2;
       break;
     default:
       throw new Error(
@@ -50,6 +84,8 @@ export function configurePositioning(face, text) {
       style.dominantBaseline = "text-before-edge";
       configuration.y -= configuration.vert - margins.vert;
       configuration.cy = configuration.y + configuration.height / 2;
+      configuration.y += diffHeight / 2;
+      configuration.cy += diffHeight / 2;
       break;
     case "middle":
       style.dominantBaseline = "central";
@@ -59,6 +95,8 @@ export function configurePositioning(face, text) {
       style.dominantBaseline = "text-after-edge";
       configuration.y += configuration.vert - margins.vert;
       configuration.cy = configuration.y - configuration.height / 2;
+      configuration.y -= diffHeight / 2;
+      configuration.cy -= diffHeight / 2;
       break;
     default:
       throw new Error(`The vertical value '${text.vertical}' is not managed`);
