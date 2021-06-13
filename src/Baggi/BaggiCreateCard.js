0d0cdc33b217609cc2e430cf3b8799cc1f176ed6
superfaz <16510828+superfaz@users.noreply.github.com>
2021-06-13 21:03:18 +0200

    Upgrade libraries

--- src/Baggi/BaggiCreateCard.js
+++ src/Baggi/BaggiCreateCard.js
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import BaseCreateCard from "../BaseTemplate/BaseCreateCard";
 
